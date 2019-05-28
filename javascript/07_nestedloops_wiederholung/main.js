c0b3bff9b55b49c8f2776e2425e1e1fd8934e355
Jan Schulz <jan.schulz@devugees.org>
2019-05-28 15:33:18 +0200

    added hausaufgabe

--- javascript/07_nestedloops_wiederholung/main.js
+++ javascript/07_nestedloops_wiederholung/main.js
@@ -34,19 +34,20 @@ for(var i=0; i<7; i++) {
 }
 
 var someNumbers = [ 
-  // 0   1   2   3 => length = 4  
-    [17, 20, 12, 6],                // 0
-  // 0   1    2    => length = 3
-    [13,-12,  0],                   // 1
-  // 0    1   2   3   4 => length = 5
-    [25,  7,  3, 10, 12],           // 2
-  // 0    1  => length = 2
-    [31, 15]                        // 3
+   // 0   1   2   3       => length = 4  
+    [17, 20, 12, 6],                // 0 
+   // 0   1   2           => length = 3
+    [13,-12,  0],                   // 1 
+   // 0   1   2   3   4   => length = 5
+    [25,  7,  3, 10, 12],           // 2 
+   // 0   1               => length = 2
+    [31, 15]                        // 3 
 ]; // length = 4
 
-console.log( someNumbers[0][0] )    // 17
+
+console.log( someNumbers[0][0] )    // 17 
 console.log( someNumbers[0][1] )    // 20 
-console.log( someNumbers[0][2] );   // 12
+console.log( someNumbers[0][2] );   // 12 
 
 for(var i=0; i<someNumbers.length; i++) {
     for(var j=0; j<someNumbers[i].length; j++) {
