e8f17f140109af92754546e99f241badcd2f085a
Jan Schulz <jan.schulz@devugees.org>
2019-06-17 10:17:21 +0200

    added onclick and addeventlistener

--- javascript/22_dom/main.js
+++ javascript/22_dom/main.js
@@ -1,1 +1,16 @@
-console.log('hallo world');
+const halloWorld1Obj = document.getElementById('halloworld1'); // halloWorldObj connected JS mit dem HTML Button
+const halloWorld2Obj = document.getElementById('halloworld2'); 
+
+halloWorldObj1.addEventListener('click', function() {
+    alert('hallo world - 1');
+});
+
+halloWorldObj2.addEventListener('click', function() {
+    alert('hallo world - 2');
+});
+
+// andere Syntax: onclick Methode überschreiben
+halloWorld2Obj.onclick = function() {
+    alert('hallo world');
+}
+
