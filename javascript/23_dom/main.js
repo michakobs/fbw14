42c92509f94fd476c7aa0cbf8af86633f3672e22
Jan Schulz <jan.schulz@devugees.org>
2019-06-18 09:37:00 +0200

    ...

---
+++ javascript/23_dom/main.js
@@ -0,0 +1,25 @@
+const greetingObj = document.getElementById('greeting');
+const sayMyNameObj = document.getElementById('saymyname');
+
+sayMyNameObj.onclick = function() {
+    // greetingObj.value gibt den value der textbox aus
+    alert(`Hallo ${greetingObj.value}`);
+}
+
+// Aufgabe:
+// 
+// 1. Erstelle zwei Textboxen und
+// einen Button mit dem Label "Addieren" 
+// 2. Wenn der Nutzer auf "Addieren" klickt, dann soll die Summe der Zahl der ersten und zweiten Textbox ausgegeben werden. Prüfe hierzu, ob in beiden Textboxen wirklich Zahlen drin stehen. Google hierzu möglichst effizient, falls Du nicht weißt wie das geht.
+
+// Schritt 1
+const summand1Obj = document.getElementById('summand1');
+const summand2Obj = document.getElementById('summand2');
+const summeObj = document.getElementById('summe');
+
+summeObj.onclick = function() {
+    // diese funktion wird aufgerufen, wenn der summe Button geklickt wird
+
+    alert(summand1Obj.value);
+    alert(summand2Obj.value);
+}
