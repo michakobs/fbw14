78c53b5d6d4d1880af981ffb4617766df79aad1e
Jan Schulz <jan.schulz@devugees.org>
2019-07-01 10:10:25 +0200

    shoppinglist -> add item

--- javascript/29_shoppinglist/main.js
+++ javascript/29_shoppinglist/main.js
@@ -1,7 +1,19 @@
 console.log('hallo world');
 
+const giftObj = document.getElementById('gift');
 const addgiftObj = document.getElementById('addgift');
 const removelastgiftObj = document.getElementById('removelastgift');
 const removefirstObj = document.getElementById('removefirst');
 const removeObj = document.getElementById('remove');
 const sumObj = document.getElementById('sum');
+const shoppinglistObj = document.getElementById('shoppinglist');
+
+addgiftObj.onclick = function() {
+    // neues li erzeugen
+    const newLiObj = document.createElement('li');
+    // den text des li setzen
+    newLiObj.innerHTML = giftObj.value;
+    // li der liste hinzufügen
+    shoppinglistObj.appendChild(newLiObj);
+    giftObj.value = '';
+}
