f33be90b81c2f03e4ec606d523e4fa3af78b9b7b
Jan Schulz <jan.schulz@devugees.org>
2019-07-01 09:57:43 +0200

    started with shoppinglist

---
+++ javascript/29_shoppinglist/main.js
@@ -0,0 +1,7 @@
+console.log('hallo world');
+
+const addgiftObj = document.getElementById('addgift');
+const removelastgiftObj = document.getElementById('removelastgift');
+const removefirstObj = document.getElementById('removefirst');
+const removeObj = document.getElementById('remove');
+const sumObj = document.getElementById('sum');
