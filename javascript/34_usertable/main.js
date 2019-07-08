aa92c8f6363e672cf7d4cb24f434feef9b75a450
Jan Schulz <jan.schulz@devugees.org>
2019-07-08 10:59:21 +0200

    added data-id to tr

--- javascript/34_usertable/main.js
+++ javascript/34_usertable/main.js
@@ -1,1 +1,44 @@
-console.log('hallo world');
+const adduserObj = document.getElementById('adduser');
+const nameObj = document.getElementById('name');
+const emailObj = document.getElementById('email');
+const usertableObj = document.getElementById('usertable');
+
+// emailObj wird immer
+// document.getElementById bleiben
+//
+// passiert NIEMALS:
+// emailObj = null; // NICHT OKAYzuweisung
+// emailObj = 3;
+//
+// emailObj.x = 3 // OK, mutation
+
+function randomString(length) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const alphaArray = alpha.split('');
+    let str = '';
+    for(let i=0;i<length;i++) {
+        str += alphaArray[ Math.floor(Math.random() * alpha.length) ];
+    }
+
+    return str;
+}
+
+adduserObj.onclick = function() {
+    const newTr = document.createElement('tr');
+    const newTdName = document.createElement('td');
+    const newTdEmail = document.createElement('td');
+    const newTdId = document.createElement('td');
+
+    newTdName.innerText = nameObj.value;
+    newTdEmail.innerText = emailObj.value;
+    const id = randomString(12);
+    newTdId.innerText = id;
+    
+    newTr.appendChild(newTdName);
+    newTr.appendChild(newTdEmail);
+    newTr.appendChild(newTdId);
+    newTr.setAttribute('data-id', id);
+
+    usertableObj.appendChild(newTr);
+}
+
