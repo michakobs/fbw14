1065b5d97f343ab13a9918812039218e77def965
Jan Schulz <jan.schulz@devugees.org>
2019-07-09 10:08:49 +0200

    started conditional rendering

---
+++ javascript/37_conditional_rendering/main.js
@@ -0,0 +1,19 @@
+const btnHomeObj = document.getElementById('btnHome');
+const btnInfoObj = document.getElementById('btnInfo');
+const btnImpressumObj = document.getElementById('btnImpressum');
+
+const homeObj = document.getElementById('home');
+const infoObj = document.getElementById('info');
+const impressumObj = document.getElementById('impessum');
+
+btnHomeObj.onclick = function() {
+    alert('1');
+}
+
+btnInfoObj.onclick = function() {
+    alert('2');
+}
+
+btnImpressumObj.onclick = function() {
+    alert('3');
+}
