6ae0f04e4c64a667114bbdf3eb1c377172234003
Jan Schulz <jan.schulz@devugees.org>
2019-07-16 11:34:42 +0200

    dry principle task added

--- javascript/44_usercomments/main.js
+++ javascript/44_usercomments/main.js
@@ -9,6 +9,15 @@ const btnShowCommentsObj = document.getElementById('btnShowComments');
 const btnAddCommentObj = document.getElementById('btnAddComment');
 const containerObj = document.getElementById('container');
 
+// DRY - Dont repeat yourself
+
+// Aufgabe:
+//
+// Schreibe eine Funktion createTable(field1, field2, field3, field4, data)
+// die eine Tabelle erzeugt mit dem Tabellenkopf aus field1, field2, field,3, field4. 
+// Der fünfte Parameter data soll gleichd dem responseText des XMLHttpRequest-Objects sein.
+
+
 btnShowUsersObj.onclick = function () {
     let url = 'http://cileria.com:3050/users';
     const xhr = new XMLHttpRequest();
@@ -84,7 +93,77 @@ btnAddUserObj.onclick = function () {
 }
 
 btnShowCommentsObj.onclick = function () {
-    containerObj.innerText = 'Show Comments';
+    let url = 'http://cileria.com:3050/comments';
+    
+    // Aufgabe: Baue eine Tabelle auf Basis
+    // der comments-Daten
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            // Aufgabe:
+            //
+            // Basierend auf den Daten, baue eine Tabelle.
+
+            const tableObj = document.createElement('table');
+            const trObj = document.createElement('tr');
+            const th1Obj = document.createElement('th');
+            const th2Obj = document.createElement('th');
+            const th3Obj = document.createElement('th');
+            const th4Obj = document.createElement('th');
+
+            th1Obj.innerText = 'Id';
+            th2Obj.innerText = 'Name';
+            th3Obj.innerText = 'Email';
+            th4Obj.innerText = 'Body';
+
+            trObj.appendChild(th1Obj);
+            trObj.appendChild(th2Obj);
+            trObj.appendChild(th3Obj);
+            trObj.appendChild(th4Obj);
+
+            tableObj.appendChild(trObj);
+
+            containerObj.innerText = '';
+
+            console.log(xhr.responseText);
+            const responseObj = JSON.parse(xhr.responseText);
+
+            // älterer iterator
+            // for(let i=0; i<responseObj.length; i++) {
+            //     console.log( responseObj[i].name );
+            // }
+
+            // neuerer iterator für arrays
+            for (let e of responseObj) {
+                const trNewObj = document.createElement('tr');
+                
+                const td1Obj = document.createElement('td');
+                const td2Obj = document.createElement('td');
+                const td3Obj = document.createElement('td');
+                const td4Obj = document.createElement('td');
+
+                td1Obj.innerText = e._id;
+                td2Obj.innerText = e.name;
+                td3Obj.innerText = e.email;
+                td4Obj.innerText = e.body;
+
+                trNewObj.appendChild(td1Obj);
+                trNewObj.appendChild(td2Obj);
+                trNewObj.appendChild(td3Obj);
+                trNewObj.appendChild(td4Obj);
+
+                tableObj.appendChild(trNewObj);
+            }
+
+            containerObj.appendChild(tableObj);
+        } 
+        else {
+
+        }
+    }
+    xhr.send();    
 }
 
 btnAddCommentObj.onclick = function () {
