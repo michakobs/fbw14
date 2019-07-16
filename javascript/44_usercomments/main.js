0f02087df4c5f8b3fd174ee6994a20fff13a58fa
Jan Schulz <jan.schulz@devugees.org>
2019-07-16 14:19:27 +0200

    finished get requests

--- javascript/44_usercomments/main.js
+++ javascript/44_usercomments/main.js
@@ -10,13 +10,69 @@ const btnAddCommentObj = document.getElementById('btnAddComment');
 const containerObj = document.getElementById('container');
 
 // DRY - Dont repeat yourself
-
 // Aufgabe:
 //
 // Schreibe eine Funktion createTable(field1, field2, field3, field4, data)
 // die eine Tabelle erzeugt mit dem Tabellenkopf aus field1, field2, field,3, field4. 
 // Der fünfte Parameter data soll gleichd dem responseText des XMLHttpRequest-Objects sein.
 
+function createTable(field1, field2, field3, field4, data) {
+    // Aufgabe:
+    //
+    // Basierend auf den Daten, baue eine Tabelle.
+
+    const tableObj = document.createElement('table');
+    const trObj = document.createElement('tr');
+    const th1Obj = document.createElement('th');
+    const th2Obj = document.createElement('th');
+    const th3Obj = document.createElement('th');
+    const th4Obj = document.createElement('th');
+
+    th1Obj.innerText = field1;
+    th2Obj.innerText = field2;
+    th3Obj.innerText = field3;
+    th4Obj.innerText = field4;
+
+    trObj.appendChild(th1Obj);
+    trObj.appendChild(th2Obj);
+    trObj.appendChild(th3Obj);
+    trObj.appendChild(th4Obj);
+
+    tableObj.appendChild(trObj);
+
+    containerObj.innerText = '';
+
+    const responseObj = JSON.parse(data);
+
+    // älterer iterator
+    // for(let i=0; i<responseObj.length; i++) {
+    //     console.log( responseObj[i].name );
+    // }
+
+    // neuerer iterator für arrays
+    for (let e of responseObj) {
+        const trNewObj = document.createElement('tr');
+
+        const td1Obj = document.createElement('td');
+        const td2Obj = document.createElement('td');
+        const td3Obj = document.createElement('td');
+        const td4Obj = document.createElement('td');
+
+        td1Obj.innerText = e[field1];
+        td2Obj.innerText = e[field2];
+        td3Obj.innerText = e[field3];
+        td4Obj.innerText = e[field4];
+
+        trNewObj.appendChild(td1Obj);
+        trNewObj.appendChild(td2Obj);
+        trNewObj.appendChild(td3Obj);
+        trNewObj.appendChild(td4Obj);
+
+        tableObj.appendChild(trNewObj);
+    }
+
+    containerObj.appendChild(tableObj);
+}
 
 btnShowUsersObj.onclick = function () {
     let url = 'http://cileria.com:3050/users';
@@ -24,64 +80,8 @@ btnShowUsersObj.onclick = function () {
     xhr.open('GET', url);
     xhr.onload = function () {
         if (xhr.status === 200) {
-            // Aufgabe:
-            //
-            // Basierend auf den Daten, baue eine Tabelle.
-
-            const tableObj = document.createElement('table');
-            const trObj = document.createElement('tr');
-            const th1Obj = document.createElement('th');
-            const th2Obj = document.createElement('th');
-            const th3Obj = document.createElement('th');
-            const th4Obj = document.createElement('th');
-
-            th1Obj.innerText = 'Id';
-            th2Obj.innerText = 'Name';
-            th3Obj.innerText = 'Username';
-            th4Obj.innerText = 'Email';
-
-            trObj.appendChild(th1Obj);
-            trObj.appendChild(th2Obj);
-            trObj.appendChild(th3Obj);
-            trObj.appendChild(th4Obj);
-
-            tableObj.appendChild(trObj);
-
-            containerObj.innerText = '';
-
-            console.log(xhr.responseText);
-            const responseObj = JSON.parse(xhr.responseText);
-
-            // älterer iterator
-            // for(let i=0; i<responseObj.length; i++) {
-            //     console.log( responseObj[i].name );
-            // }
-
-            // neuerer iterator für arrays
-            for (let e of responseObj) {
-                const trNewObj = document.createElement('tr');
-                
-                const td1Obj = document.createElement('td');
-                const td2Obj = document.createElement('td');
-                const td3Obj = document.createElement('td');
-                const td4Obj = document.createElement('td');
-
-                td1Obj.innerText = e._id;
-                td2Obj.innerText = e.name;
-                td3Obj.innerText = e.username;
-                td4Obj.innerText = e.email;
-
-                trNewObj.appendChild(td1Obj);
-                trNewObj.appendChild(td2Obj);
-                trNewObj.appendChild(td3Obj);
-                trNewObj.appendChild(td4Obj);
-
-                tableObj.appendChild(trNewObj);
-            }
-
-            containerObj.appendChild(tableObj);
-        } 
-        else {
+            createTable('_id', 'name', 'username', 'email', xhr.responseText);
+        } else {
 
         }
     }
@@ -94,76 +94,20 @@ btnAddUserObj.onclick = function () {
 
 btnShowCommentsObj.onclick = function () {
     let url = 'http://cileria.com:3050/comments';
-    
+
     // Aufgabe: Baue eine Tabelle auf Basis
     // der comments-Daten
-    
+
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = function () {
         if (xhr.status === 200) {
-            // Aufgabe:
-            //
-            // Basierend auf den Daten, baue eine Tabelle.
-
-            const tableObj = document.createElement('table');
-            const trObj = document.createElement('tr');
-            const th1Obj = document.createElement('th');
-            const th2Obj = document.createElement('th');
-            const th3Obj = document.createElement('th');
-            const th4Obj = document.createElement('th');
-
-            th1Obj.innerText = 'Id';
-            th2Obj.innerText = 'Name';
-            th3Obj.innerText = 'Email';
-            th4Obj.innerText = 'Body';
-
-            trObj.appendChild(th1Obj);
-            trObj.appendChild(th2Obj);
-            trObj.appendChild(th3Obj);
-            trObj.appendChild(th4Obj);
-
-            tableObj.appendChild(trObj);
-
-            containerObj.innerText = '';
-
-            console.log(xhr.responseText);
-            const responseObj = JSON.parse(xhr.responseText);
-
-            // älterer iterator
-            // for(let i=0; i<responseObj.length; i++) {
-            //     console.log( responseObj[i].name );
-            // }
-
-            // neuerer iterator für arrays
-            for (let e of responseObj) {
-                const trNewObj = document.createElement('tr');
-                
-                const td1Obj = document.createElement('td');
-                const td2Obj = document.createElement('td');
-                const td3Obj = document.createElement('td');
-                const td4Obj = document.createElement('td');
-
-                td1Obj.innerText = e._id;
-                td2Obj.innerText = e.name;
-                td3Obj.innerText = e.email;
-                td4Obj.innerText = e.body;
-
-                trNewObj.appendChild(td1Obj);
-                trNewObj.appendChild(td2Obj);
-                trNewObj.appendChild(td3Obj);
-                trNewObj.appendChild(td4Obj);
-
-                tableObj.appendChild(trNewObj);
-            }
-
-            containerObj.appendChild(tableObj);
-        } 
-        else {
+            createTable('_id', 'name', 'email', 'body', xhr.responseText);
+        } else {
 
         }
     }
-    xhr.send();    
+    xhr.send();
 }
 
 btnAddCommentObj.onclick = function () {
