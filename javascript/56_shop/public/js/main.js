ad9cdadb908847fbbf186c8808ae7541b2c6aada
Jan Schulz <jan.schulz@devugees.org>
2019-08-20 10:09:02 +0200

    still missing checkout button

---
+++ javascript/56_shop/public/js/main.js
@@ -0,0 +1,36 @@
+const thumbnailObj = document.getElementsByClassName('thumbnail');
+
+const thumbnailContainerObj = document.getElementById('thumbnail-container');
+
+const productDetailsObj = document.getElementById('product-details');
+
+const cartObj = document.getElementById('cart');
+
+const thankyouObj = document.getElementById('thankyou');
+
+// in den Warenkorb legen - Button
+const putItemCartObj = document.getElementById('put-item-cart');
+
+const showProductDetails = () => {
+    productDetailsObj.style.display = 'block';
+    thumbnailContainerObj.style.display = 'none';        
+    cartObj.style.display = 'none';
+    thankyouObj.style.display = 'none';    
+}
+
+const showCart = () => {
+    productDetailsObj.style.display = 'none';
+    thumbnailContainerObj.style.display = 'none';        
+    cartObj.style.display = 'block';
+    thankyouObj.style.display = 'none';    
+}
+
+putItemCartObj.onclick = () => {
+    showCart();
+}
+
+for(let i=0; i<thumbnailObj.length; i++) {
+    thumbnailObj[i].onclick = () => {
+        showProductDetails();
+    }
+}
