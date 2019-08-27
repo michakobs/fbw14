493a052090153ecc05430f8306ccc2d196d74976
Jan Schulz <jan.schulz@devugees.org>
2019-08-27 11:11:07 +0200

    added loadProducts

--- javascript/56_shop/public/js/main.js
+++ javascript/56_shop/public/js/main.js
@@ -9,35 +9,52 @@ const logoObj = document.getElementById('logo');
 // in den Warenkorb legen - Button
 const putItemCartObj = document.getElementById('put-item-cart');
 
-const loadProducts = () => {
+const loadProducts = async () => {
     // 1. fetch-aufruf abfeuern
     const response = await fetch('http://localhost:3000/product');
     // 2. das objekt als json interpretieren -> wir bekommen ein objekt
     const responseJson = await response.json();
-    // 3. als string darstellen
-    const responseStr = JSON.stringify(responseJson);
-    console.log(responseStr);        
+    
+    // <div class="thumbnail">
+    //    <div class="thumbnail-name">Beethoven</div>
+    //    <div class="thumbnail-price">100 €</div>
+    // </div>    
+    
+    thumbnailContainerObj.innerHTML = '';
+
+    for(product of responseJson.products) {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'thumbnail';
+        newDiv.style.background = `url(public/${product.imgurl}) center center`;
+        newDiv.innerHTML = `
+           <div class="thumbnail-name">${product.name}</div>
+           <div class="thumbnail-price">${product.price} €</div>            
+        `;
+        thumbnailContainerObj.appendChild(newDiv);
+    }
 }
+
+loadProducts();
 
 const showStartpage = () => {
     productDetailsObj.style.display = 'none';
     thumbnailContainerObj.style.display = 'flex';
     cartObj.style.display = 'none';
-    thankyouObj.style.display = 'none';    
+    thankyouObj.style.display = 'none';
 }
 
 const showProductDetails = () => {
     productDetailsObj.style.display = 'block';
-    thumbnailContainerObj.style.display = 'none';        
+    thumbnailContainerObj.style.display = 'none';
     cartObj.style.display = 'none';
-    thankyouObj.style.display = 'none'   
+    thankyouObj.style.display = 'none'
 }
 
 const showCart = () => {
     productDetailsObj.style.display = 'none';
     thumbnailContainerObj.style.display = 'none';
     cartObj.style.display = 'block';
-    thankyouObj.style.display = 'none'; 
+    thankyouObj.style.display = 'none';
 }
 
 const showThankYou = () => {
@@ -51,7 +68,7 @@ const showThankYou = () => {
         display: flex
     }
     */
-    thankyouObj.style.display = 'flex';    
+    thankyouObj.style.display = 'flex';
 }
 
 putItemCartObj.onclick = () => {
@@ -62,7 +79,7 @@ cartBuyButtonObj.onclick = () => {
     showThankYou();
 }
 
-for(let i=0; i<thumbnailObj.length; i++) {
+for (let i = 0; i < thumbnailObj.length; i++) {
     thumbnailObj[i].onclick = () => {
         showProductDetails();
     }
