9205e15c8753b8506c0fb6151d9b95c838978e18
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-09-05 15:31:12 +0200

    React

--- react/01_helloreact/hello-react/src/App.js
+++ react/01_helloreact/hello-react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import ImageName from './katze.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ImageName} className="App-logo" alt="logo" />
         <p>
           Hallo Welt!2222 kjhjkhk asdasd lkasldfkams
           Edit <code>src/App.js</code> and save to reload.
